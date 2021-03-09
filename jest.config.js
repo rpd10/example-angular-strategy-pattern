01a2f151af4d1681198e7b25ac9a7e7c0df30dd7
Ryan Diehl <rpd10@psu.edu>
2021-03-09 13:10:52 -0500

    chore: update to latest angular

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
+  projects: [
+    '<rootDir>/apps/demo',
+    '<rootDir>/libs/login',
+    '<rootDir>/libs/my-feature',
+    '<rootDir>/libs/auth',
+    '<rootDir>/libs/fancy-auth-provider',
+  ],
 };
--- libs/my-feature/src/lib/my-feature.component.spec.ts
+++ libs/my-feature/src/lib/my-feature.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MyFeatureComponent } from './my-feature.component';
 
@@ -6,7 +6,7 @@ describe('MyFeatureComponent', () => {
   let component: MyFeatureComponent;
   let fixture: ComponentFixture<MyFeatureComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyFeatureComponent ]
     })
